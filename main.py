06ed974ac3a0736e4ecdd12ad029ce3f0a8d6baf
Tristan DE KERAUTEM <tristan.dekerautem@jdc.fr>
2025-10-24 08:36:14 +0200

    Fix: rendre l'import POSIX portable (CLD_CONTINUED) + update main.py plus génération de différentes configurations de nb de joueurs

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import math
 
 if __name__ == "__main__":
     print("debut")
+    nbInscris=27
+    nbSeed=4
     myPossiblePools = PoolConfigurationGenerator()
     myPossiblePools.displayPoolFullConfigurations()
-    print(myPossiblePools.get_pool_sizes_list(23))
-    mes_joueurs = creation_joueurs(23, 4)
+    print(myPossiblePools.get_pool_sizes_list(nbInscris))
+    mes_joueurs = creation_joueurs(nbInscris, nbSeed)
     for joueur in mes_joueurs:
         print(joueur.prenom + " " + joueur.nom + "-" + str(joueur.niveau) + "-" + str(joueur.age) + "-" + str(joueur.tete_de_serie))
     # tester la génération de poules imposées
-    tailles_poules = myPossiblePools.get_pool_sizes_list(23)
-    ma_configuration_poule = myPossiblePools.plan_for_N_with_caps(23)
+    tailles_poules = myPossiblePools.get_pool_sizes_list(nbInscris)
+    ma_configuration_poule = myPossiblePools.plan_for_N_with_caps(nbInscris)
     print(f"le nombre de gagnant par poule:{ma_configuration_poule['Gagnants par poule (format 221)']}") 
     # ajouter une méthode qui calcul le nombre de gagant par poule 
     ## Simulation des gagnants de la poule et de la consolante
@@ -21,8 +23,11 @@ if __name__ == "__main__":
     print(f"joueurs têtes de série : {joueurs_tete_de_serie}")
     # mettre dans les poules uniquement les joueurs non tetes de série lesquels intégrent directement le tournoi principal
     repartiteur = RepartiteurPoulesFixes( joueurs_standard, tailles_poules)
-    mes_poules = repartiteur.repartir_par_backtracking()
+   # mes_poules = repartiteur.repartir_par_backtracking()
     #mes_poules = repartiteur.repartir_par_recherche_locale()
+    repartiteur.repartir_par_couts_TK(joueurs_standard)
+    mes_poules = repartiteur.poules
+   
     for i,poule in enumerate(mes_poules, start=1):
         print(f'Poule {i}: {poule}')
 
@@ -31,5 +36,8 @@ if __name__ == "__main__":
 ## créer le tableau pour le premier tour 
 ## crééer le tableau pour la consolante (avec joueurs by)
 ## prévoir de faire une tableau de consolante en placant avantageusement ceux qui ont le plus de point = le moins de jeu perdus ou bien la différence entre le nombre de match perdu et le nomnre de match gagné
-    
+## Exporter les résultats dans un fichier excel
+## exporter les résultats dans un fichier json
+## afficher les résultats dans un tableau html
+
 print("fin")
